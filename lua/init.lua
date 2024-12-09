11d4b88cbfa2c31c43e7f72cc1f78526d6ef56e3
grommoott <grommoott@gmail.com>
2024-12-10 02:54:36 +0700

    fix

--- lua/init.lua
+++ lua/init.lua
@@ -2,7 +2,7 @@ local M = {}
 
 -- @param opts? worstysearchy.Config
 local function setup(opts)
-	require("./worstysearchy.lua").Init(opts)
+	require("worstysearchy.lua").Init(opts)
 end
 
 M.setup = setup
