4e746962726d29823bdd48a12e86b67aeb68f65d
grommoott <grommoott@gmail.com>
2024-12-12 00:28:07 +0700

    main

--- lua/worstysearchy/init.lua
+++ lua/worstysearchy/init.lua
@@ -1,13 +1,8 @@
 local M = {}
 
-local function init(opts)
-	vim.print("Hello world!")
-	-- require("worstysearchy.worstysearchy").Init(opts)
-end
-
 -- @param opts? worstysearchy.Config
 function M.setup(opts)
-	vim.notify("Hello world!", vim.log.levels.INFO, { title = "WorstySearchy" })
+	require("worstysearchy.worstysearchy").Init(opts)
 end
 
 return M
